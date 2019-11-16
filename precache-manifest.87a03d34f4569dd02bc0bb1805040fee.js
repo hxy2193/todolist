e8864d0c73e6e0eb7d1ccb194b41e92e26979320
hanxiaoyang <hanxiaoyang@quannengkuaiche.cn>
2019-11-16 21:19:42 +0800

    Updates

---
+++ precache-manifest.87a03d34f4569dd02bc0bb1805040fee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a5a1941f7cab3dbd3d7b63f9ef89d4",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "c4b3daf634346f1395b5",
+    "url": "/todolist/static/css/2.44ce4128.chunk.css"
+  },
+  {
+    "revision": "0e367c99a46e88859ade",
+    "url": "/todolist/static/css/main.19cbff7d.chunk.css"
+  },
+  {
+    "revision": "c4b3daf634346f1395b5",
+    "url": "/todolist/static/js/2.44076e43.chunk.js"
+  },
+  {
+    "revision": "0e367c99a46e88859ade",
+    "url": "/todolist/static/js/main.fec53069.chunk.js"
+  },
+  {
+    "revision": "133e9b3a1ce83521afa0",
+    "url": "/todolist/static/js/runtime-main.6b45bc08.js"
+  }
+]);
